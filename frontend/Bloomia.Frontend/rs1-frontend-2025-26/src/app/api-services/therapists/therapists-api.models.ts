250aa25b48d693107b9aee4247b826413d904f71
Irena Sabic <irenasabiic@gmail.com>
2025-12-28 15:00:36 +0100

    Added therapist API models

---
+++ frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/api-services/therapists/therapists-api.models.ts
@@ -0,0 +1,57 @@
+import { BasePagedQuery } from "../../core/models/paging/base-paged-query";
+import { PageResult } from "../../core/models/paging/page-result";
+
+export class ListTherapistsRequest extends BasePagedQuery {
+    firstname?: string | null;
+    lastname?: string | null;
+    specialiation?: string | null;
+    genderId?: number | null;
+    sortByRatingDesc?: boolean;
+}
+
+export interface ListTherapistsQueryDto {
+    id: number;
+    fullname?: string | null;
+    specialization?: string | null;
+    ratingAvg: number;
+    profileImage?: string | null;
+    gender?: string | null;
+}
+
+export type ListTherapistsResponse = PageResult<ListTherapistsQueryDto>;
+
+export interface GetTherapistByIdQueryDto {
+    id: number;
+    firstname?: string | null;
+    lastname?: string | null;
+    username?: string | null;
+    fullname?: string | null;
+    email?: string | null;
+    phoneNumber?: string | null;
+    profileImage?: string | null;
+    specialization?: string | null;
+    description?: string | null;
+    ratingAvg: number;
+    isVerified: boolean | null;
+    documentName?: string | null;
+    therapyTypes: TherapyTypeDto[],
+    availability: TherapistAvailabilityDto[];
+}
+
+export interface TherapyTypeDto {
+    id: number;
+    name?: string | null;
+}
+
+export interface TherapistAvailabilityDto {
+    id: number;
+    date: string;
+    startTime: string;
+    isbooked?: boolean | null;
+} 
+
+export interface ChangeTherapistPasswordCommand {
+    id: number;
+    currentPassword?: string | null;
+    newPassword?: string | null;
+}
--- frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/core/services/auth/jwt-payload.dto.ts
+++ frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/core/services/auth/jwt-payload.dto.ts
@@ -8,6 +8,8 @@ export interface JwtPayloadDto {
   fullname?: string;
   // backend returns a single role string like "CLIENT", "THERAPIST", "ADMIN"
   role?: string;
+  'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'?: string;
+  
   ver: string;
   iat: number;
   exp: number;
