a5186594d99e1339c692d4809be5a3d4b13ac2cc
Irena Sabic <irenasabiic@gmail.com>
2025-12-28 15:20:43 +0100

    Add therapists API service

--- frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/api-services/therapists/therapists-api.service.ts
+++ frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/api-services/therapists/therapists-api.service.ts
@@ -1,5 +1,11 @@
 import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+import { Observable } from "rxjs";
+import { ListTherapistsRequest, ListTherapistsResponse, GetTherapistByIdQueryDto,
+    ChangeTherapistPasswordCommand, UpdateTherapistCommand
+ } from "./therapists-api.models";
+ import { buildHttpParams } from "../../core/models/build-http-params";
 
 @Injectable({
     providedIn: 'root'
@@ -8,4 +14,22 @@ import { HttpClient } from "@angular/common/http";
 export class TherapistsApiService {
     private readonly baseUrl = '${environment.apiUrl}/therapists';
     private http = inject(HttpClient);
+
+    list(request?: ListTherapistsRequest): Observable<ListTherapistsResponse> {
+        const params = request ? buildHttpParams(request as any) : undefined;
+        
+        return this.http.get<ListTherapistsResponse>(this.baseUrl, { params });
+    }
+
+    getById(id: number): Observable<GetTherapistByIdQueryDto> {
+        return this.http.get<GetTherapistByIdQueryDto>(`${this.baseUrl}/${id}`);
+    }
+
+    update(id: number, payload: UpdateTherapistCommand): Observable<void> {
+        return this.http.put<void>(`${this.baseUrl}/${id}`, payload);
+    }
+
+    changePassword(id:number, payload: ChangeTherapistPasswordCommand): Observable<void> {
+        return this.http.put<void>(`${this.baseUrl}/${id}/change-password`, payload);
+    }
 }
