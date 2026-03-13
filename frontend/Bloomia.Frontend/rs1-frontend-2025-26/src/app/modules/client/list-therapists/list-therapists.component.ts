c9d54c0cabbb5316ba8cec2e02a4c62e2b0160de
Lejla Nezirovic <133971986+lejlanezirovic@users.noreply.github.com>
2026-03-13 12:57:05 +0100

    updated responsive site design, added angular material icons

--- frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/modules/client/list-therapists/list-therapists.component.ts
+++ frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/app/modules/client/list-therapists/list-therapists.component.ts
@@ -14,6 +14,7 @@ import { ToasterService } from '../../../core/services/toaster.service';
   styleUrl: './list-therapists.component.scss',
 })
 export class ListTherapistsComponent implements OnInit {
+//ima paging mozemo implementirati
 
   private router=inject(Router);
   private apiService=inject(TherapistsApiService);
