56ab1f446a401c54687ac5a54afc9e964b3c8623
Lejla Nezirovic <133971986+lejlanezirovic@users.noreply.github.com>
2026-03-14 16:44:46 +0100

    firebase notifications setup

--- frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/environments/environment.ts
+++ frontend/Bloomia.Frontend/rs1-frontend-2025-26/src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7260'
+  apiUrl: 'https://localhost:7260',
+
+  firebase:{
+    apiKey: "AIzaSyCNi3zscgZiNx4zvu5Y6cp_3dDtPhVTIgY",
+    authDomain: "bloomia-notifications.firebaseapp.com",
+    projectId: "bloomia-notifications",
+    storageBucket: "bloomia-notifications.firebasestorage.app",
+    messagingSenderId: "681467025172",
+    appId: "1:681467025172:web:fe1cd56423784a18145401",
+    measurementId: "G-WK1TEJ1ZJ8"
+  }
 };
