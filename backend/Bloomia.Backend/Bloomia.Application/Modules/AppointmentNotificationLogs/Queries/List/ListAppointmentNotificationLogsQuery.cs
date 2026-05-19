ba126afbb09a26dec6c58252c8d31406df2e670b
Irena Sabic <irenasabiic@gmail.com>
2026-05-19 10:15:54 +0200

    Add endpoint for listing appointment reminder logs

---
+++ backend/Bloomia.Backend/Bloomia.Application/Modules/AppointmentNotificationLogs/Queries/List/ListAppointmentNotificationLogsQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloomia.Application.Modules.AppointmentNotificationLogs.Queries.List
+{
+    public class ListAppointmentNotificationLogsQuery : IRequest<List<ListAppointmentNotificationLogsQueryDto>>
+    {
+    }
+}
---
+++ backend/Bloomia.Backend/Bloomia.Application/Modules/AppointmentNotificationLogs/Queries/List/ListAppointmentNotificationLogsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloomia.Application.Modules.AppointmentNotificationLogs.Queries.List
+{
+    public class ListAppointmentNotificationLogsQueryHandler(IAppDbContext context) : IRequestHandler<ListAppointmentNotificationLogsQuery, List<ListAppointmentNotificationLogsQueryDto>>
+    {
+        public async Task<List<ListAppointmentNotificationLogsQueryDto>> Handle(ListAppointmentNotificationLogsQuery request, CancellationToken ct)
+        {
+            var logs = await context.AppointmentNotificationLogs
+                        .AsNoTracking()
+                        .Include(x => x.Appointment)
+                            .ThenInclude(x => x.Client)
+                                .ThenInclude(x => x.User)
+                        .Include(x => x.Appointment)
+                            .ThenInclude(x => x.TherapistAvailability)
+                                .ThenInclude(x => x.Therapist)
+                                    .ThenInclude(x => x.User)
+                        .OrderByDescending(x => x.CreatedAtUtc)
+                        .Select(x => new ListAppointmentNotificationLogsQueryDto
+                        {
+                            Id = x.Id,
+                            AppointmentId = x.AppointmentId,
+                            RecipientEmail = x.RecipientEmail,
+                            NotificationType = x.NotificationType.ToString(),
+                            Status = x.Status.ToString(),
+                            CreatedAtUtc = x.CreatedAtUtc,
+                            SentAtUtc = x.SentAtUtc,
+                            ErrorMessage = x.ErrorMessage,
+                            ClientName = x.Appointment.Client.User.Fullname ??
+                                        $"{x.Appointment.Client.User.Firstname} {x.Appointment.Client.User.Lastname}".Trim(),
+                            TherapistName = x.Appointment.TherapistAvailability.Therapist.User.Fullname ??
+                                        $"{x.Appointment.TherapistAvailability.Therapist.User.Firstname} {x.Appointment.TherapistAvailability.Therapist.User.Lastname}".Trim(),
+                            ScheduledAtUtc = x.Appointment.ScheduledAtUtc,
+                            SessionType = x.Appointment.SessionType.ToString()
+                        }).ToListAsync(ct);
+
+            return logs;
+        }
+    }
+}
